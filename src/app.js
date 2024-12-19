e0e09455a86e83356028ca7e33e9219d8426757e
abdul <taylorkings30@gmail.com>
2024-12-19 16:52:24 +0500

    intail commit

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.title),
+    React.createElement("p", {}, props.description),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "padre gino's"),
+    React.createElement(pizza, {
+      title: "Pepperoni",
+      description: "Pepperoni, cheese, tomato sauce",
+    }),
+    React.createElement(pizza, {
+      title: "Margherita",
+      description: "Mozzarella, tomato sauce, basil",
+    }),
+    React.createElement(pizza, {
+      title: "Hawaiian",
+      description: "Pineapple, ham, cheese, tomato sauce",
+    }),
+    React.createElement(pizza, {
+      title: "BBQ Chicken",
+      description: "BBQ sauce, chicken, cheese, onion",
+    }),
+    React.createElement(pizza, {
+      title: "Vegetarian",
+      description: "Mushrooms, bell peppers, olives, cheese, tomato sauce",
+    }),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
