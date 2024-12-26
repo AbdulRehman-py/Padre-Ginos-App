0b50e8ad09e7c19b40fb069699ef8d7b5327bb26
abdul <taylorkings30@gmail.com>
2024-12-26 08:57:33 +0500

    tanstack query setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import prettier from "eslint-config-prettier";
 import reactPlugin from "eslint-plugin-react";
+import pluginQuery from "@tanstack/eslint-plugin-query";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -17,6 +18,7 @@ export default [
   reactPlugin.configs.flat["jsx-runtime"],
   {
     files: ["**/*.js", "**/*.jsx"],
+    ...pluginQuery.configs["flat/recommended"],
     languageOptions: {
       globals: { ...globals.browser, ...globals.node },
       parserOptions: {
